584535ad1e0e34b7fc792e180f746b8b1293c4ef
Marcin831 <balcerzak.marcin@windowslive.com>
2023-12-09 17:07:03 +0100

    Czień piąty

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,17 +1,70 @@
-﻿string name = "Ewa";
-var sex = "Kobieta";
-int age = 29;
+﻿int number = 456673921;
+string numberInString = number.ToString();
+char[] letters = numberInString.ToArray();
 
-if (name == "Ewa" && age == 33)
+int numberOf0 = 0;
+int numberOf1 = 0;
+int numberOf2 = 0;
+int numberOf3 = 0;
+int numberOf4 = 0;
+int numberOf5 = 0;
+int numberOf6 = 0;
+int numberOf7 = 0;
+int numberOf8 = 0;
+int numberOf9 = 0;
+
+foreach (var i in letters)
 {
-    Console.WriteLine("Ewa, lat 33");
+    if (i == '0')
+    {
+        numberOf0++;
+    }
+    else if (i == '1')
+    {
+        numberOf1++;
+    }
+    else if (i == '2')
+    {
+        numberOf2++;
+    }
+    else if (i == '3')
+    {
+        numberOf3++;
+    }
+    else if (i == '4')
+    {
+        numberOf4++;
+    }
+    else if (i == '5')
+    {
+        numberOf5++;
+    }
+    else if (i == '6')
+    {
+        numberOf6++;
+    }
+    else if (i == '7')
+    {
+        numberOf7++;
+    }
+    else if (i == '8')
+    {
+        numberOf8++;
+    }
+    else if (i == '9')
+    {
+        numberOf9++;
+    }
 }
-else if (sex == "Kobieta" && age < 30)
-{
-    Console.WriteLine("Kobieta poniżej 30 lat");
-}
-else if (sex != "Kobieta" && age < 18)
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
-}
-   
+
+Console.WriteLine("Wyniki dla liczby:" + number);
+Console.WriteLine("0 = " + numberOf0);
+Console.WriteLine("1 = " + numberOf1);
+Console.WriteLine("2 = " + numberOf2);
+Console.WriteLine("3 = " + numberOf3);
+Console.WriteLine("4 = " + numberOf4);
+Console.WriteLine("5 = " + numberOf5);
+Console.WriteLine("6 = " + numberOf6);
+Console.WriteLine("7 = " + numberOf7);
+Console.WriteLine("8 = " + numberOf8);
+Console.WriteLine("9 = " + numberOf9);
